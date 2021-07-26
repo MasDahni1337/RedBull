0c0d75af26c39a9ce3d6bcb5945176ad0b49f093
MasDahni1337 <ekodahni@gmail.com>
2021-07-26 15:00:52 +0700

    main file

---
+++ redbull.py
@@ -0,0 +1,48 @@
+import os
+from android import *
+from posix import PRIO_PGRP
+
+def awal():
+    os.system('neofetch --ascii /home/dahni/ascii/bull1.txt')
+    print("W3lcome to RedBull RAT Tools")
+    print("[1] Build virus server")
+    print("[2] Bind server to apk/exe")
+    print("[3] Exploit victim")
+
+def banner():
+    print("Chose your exploit method!")
+    print("[1] Android")
+    print("[2] Windows")
+    print("[3] Linux")
+    print("[4] iOS")
+    print("[5] MacOS")
+    print("")
+
+def bind():
+    print("Chose your bind object!")
+    print("[1] Original Apk")
+    print("[2] Exe Windows")
+
+def eksekusi():
+    awal()
+    x = input("chose : ")
+    load()
+    if x == "1":
+        banner()
+        pilih = input("chose : ")
+        load()
+        if pilih == "1":
+            ban_andro()
+            return eksekusi()
+    elif x == "2":
+        bind()
+        pilih = input("chose : ")
+        if pilih == "1":
+            bind_apk()
+            return eksekusi()
+    elif x == "3":
+        print("this for exploit function")
+    else:
+        print("masukan input yg bener")
+
+eksekusi()
